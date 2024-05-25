046c3784e30764a433b47f58b35f9b52c9e5523b
Patryk Murawski <patryk.murawski@booming-games.com>
2024-05-25 12:57:34 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
         <App />
-        </HashRouter>
+        </BrowserRouter>
      
     </React.StrictMode>,
 )
